5f3ee5a2ccfad5f02edc1909a8cf547ec610360b
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-08-20 19:57:07 +0300

    Full-screen gallery

--- app/jsx/photos.jsx
+++ app/jsx/photos.jsx
@@ -19,20 +19,29 @@ var PhotosIndex = React.createClass({
 
   render: function() {
     return (
-      <section className="col-md-9 photos">
-        <Loader loaded={this.state.loaded} color="#fff">
-          <ul className="list-unstyled row">
-          {this.state.items.map(function(item) {
-            return (
-              <li key={item.id} className="col-xs-6 col-sm-4 col-md-4 col-lg-3">
-                <Photo item={item} />
-                <p class="text-muted">{item.title}</p>
-              </li>
-            )
-          }.bind(this))}
-          </ul>
-        </Loader>
-      </section>
+      <div>
+        <section className="col-md-9 photos">
+          <Loader loaded={this.state.loaded} color="#fff">
+            <ul className="list-unstyled row">
+            {this.state.items.map(function(item) {
+              return (
+                <li key={item.id} className="col-xs-6 col-sm-4 col-md-4 col-lg-3">
+                  <Photo type="thumbnail" item={item} />
+                  <p className="text-muted">{item.title}</p>
+                </li>
+              )
+            }.bind(this))}
+            </ul>
+          </Loader>
+        </section>
+        <section className="fotorama" data-auto="false">
+        {this.state.items.map(function(item) {
+          return (
+            <Photo key={item.id} type="original" item={item} />
+          )
+        }.bind(this))}
+        </section>
+      </div>
     );
   }
 });
@@ -50,15 +59,44 @@ var PhotoShow = React.createClass({
 var Photo = React.createClass({
   propTypes: {
     item: React.PropTypes.object.isRequired,
+    type: React.PropTypes.string.isRequired
+  },
+
+  id: function() {
+    return "photo-" + this.props.item.id;
+  },
+
+  click: function() {
+    $(".fotorama").show().fotorama({
+      width: "100%",
+      height: "100%",
+      nav: "thumbs",
+      keyboard: true
+    });
+    $(".fotorama").data("fotorama").show(this.id());
+
+    $(document).bind("keydown", this.keydown);
+  },
+
+  keydown: function(evt) {
+    if(evt.keyCode == 27) {
+      $(".fotorama").hide();
+    }
+  },
+
+  componentWillUnmount: function() {
+    $(document).unbind("keydown", this.keydown);
+    $(".fotorama").data("fotorama").destroy();
   },
 
   render: function() {
+    var thumb = "/static/thumbnail/" + this.props.item.basename;
     var style = {
-      "background-image": "url(/static/thumbnail/" + this.props.item.basename + ")"
+      "background-image": "url(/static/" + this.props.type + "/" + this.props.item.basename + ")"
     };
 
     return (
-      <Link to="photo" id={this.props.item.id} style={style}></Link>
+      <div style={style} id={this.id()} data-thumb={thumb} onClick={this.click}>-</div>
     );
   }
 });
